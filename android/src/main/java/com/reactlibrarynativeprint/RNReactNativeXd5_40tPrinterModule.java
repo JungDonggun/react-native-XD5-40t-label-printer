cc82843be24f17e2dd16b20464d4e672f26bdefd
JungDonggun <donggun.dev@gmail.com>
2020-08-24 19:08:58 +0900

    Fix Error to "Can't create handler inside thread Thread[Thread-6,5,main] that has not called Looper.prepare()"

--- android/src/main/java/com/reactlibrarynativeprint/RNReactNativeXd5_40tPrinterModule.java
+++ android/src/main/java/com/reactlibrarynativeprint/RNReactNativeXd5_40tPrinterModule.java
@@ -1,4 +1,3 @@
-
 package com.reactlibrarynativeprint;
 
 import android.util.Log;
@@ -7,23 +6,72 @@ import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
+import com.facebook.react.ReactInstanceManager;
+
+import android.os.Handler;
+import android.os.Message;
+import android.os.Looper;
+
+import com.bixolon.labelprinter.BixolonLabelPrinter;
 
 public class RNReactNativeXd5_40tPrinterModule extends ReactContextBaseJavaModule {
+    private ReactApplicationContext reactContext;
+    private BixolonLabelPrinter mBixolonLabelPrinter;
 
-  private final ReactApplicationContext reactContext;
+    /*public Handler mHandler = new Handler(Looper.getMainLooper()) {
+        @Override
+        public void handleMessage(Message message) {
+            // This is where you do your work in the UI thread.
+            // Your worker tells you in the message what to do.
+        }
+    };*/
 
-  public RNReactNativeXd5_40tPrinterModule(ReactApplicationContext reactContext) {
-    super(reactContext);
-    this.reactContext = reactContext;
-  }
 
-  @Override
-  public String getName() {
-    return "RNReactNativeXd5_40tPrinter";
-  }
+    public RNReactNativeXd5_40tPrinterModule(ReactApplicationContext reactContext) {
+        super(reactContext);
+        this.reactContext = reactContext;
 
-  @ReactMethod
-  public void show() {
-    Log.i("JUNGDONGGUN", "SUCCESS");
-  }
+        thread.start();
+    };
+
+    Thread thread = new Thread(new Runnable() {
+        @Override
+        public void run() {
+            Looper.prepare();
+            mBixolonLabelPrinter = new BixolonLabelPrinter(null, null, null);
+        }
+    });
+
+
+    @Override
+    public String getName() {
+        return "RNReactNativeXd5_40tPrinter";
+    };
+
+    @ReactMethod
+    public synchronized void findBluetoothPrinters() {
+        mBixolonLabelPrinter.findBluetoothPrinters();
+    };
+
+    @ReactMethod
+    public synchronized void connectPrinter(String address) {
+        mBixolonLabelPrinter.connect(address);
+    };
+
+//  @ReactMethod
+//  public void draw1dBarcode() {
+//    Draw1dBarcodeActivity drawBarcode = new Draw1dBarcodeActivity();
+//    boolean status = false;
+//
+//    try {
+//      drawBarcode.print1dBarcode();
+//      status = true;
+//    } catch (Exception error) {
+//      Log.i("react-native-xd5-405-label-printer Error", error.toString());
+//    }
+//
+//
+//    Log.i("react-native-xd5-405-label-printer Status", status);
+//
+//  }
 }
